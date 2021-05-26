75aa8a1eeed6007f5b1c8f2ea947082da56528be
Nkateko <tekonku@icloud.com>
2021-05-26 14:14:16 +0200

    done

--- tests/bicycles/BicycleSpecificationTests.java
+++ tests/bicycles/BicycleSpecificationTests.java
@@ -1,8 +1,9 @@
 package bicycles;
 
 import bicycles.*;
-import bicycles.models.RoadBike;
 import bicycles.models.MountainBike;
+import bicycles.models.RoadBike;
+import bicycles.models.Tandem;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,46 +11,98 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BicycleSpecificationTests {
 
         @Test
-        public void shouldTakeMountainBikeRideOne() {
-            //constructor has 3 parameters
-            BicycleSpecification bicycleSpec = new BicycleSpecification(5,3,BicycleType.MountainBike);
-            Bicycle bicycle = new BicycleFromSpec(bicycleSpec);
-            BikeRide bikeRide = new BikeRide(bicycle);
-            bikeRide.ride(bicycle);
-            assertEquals(5, bicycle.currentSpeed());
-
-        }
-        @Test
-        public void shouldTakeMountainBikesRideTwo() {
+        public void shouldTakeMountainBikesRideOne() {
 
             //constructor has 3 parameters
             BicycleSpecification bicycleSpec = new BicycleSpecification(5,3,BicycleType.MountainBike);
-            Bicycle bicycle = new BicycleFromSpec(bicycleSpec);
-            BikeRideTwo bikeRide = new BikeRideTwo(bicycle);
-            bikeRide.ride(bicycle);
-            assertEquals(10, bicycle.currentSpeed());
+            Bicycle bicycleSpecification = new BicycleFromSpec(bicycleSpec);
+            Bicycle bicycle = new MountainBike();
+            BikeRide bikeRide= new BikeRide(bicycle);
+            bikeRide.ride(bicycleSpecification);
+            assertEquals(5, bikeRide.currentSpeed());
         }
+    @Test
+    public void shouldTakeMountainBikeRideTwo() {
+        //constructor has 3 parameters
+        BicycleSpecification bicycleSpec = new BicycleSpecification(5,3,BicycleType.MountainBike);
+        Bicycle bicycleSpecification = new BicycleFromSpec(bicycleSpec);
+        Bicycle bicycle = new MountainBike();
+        BikeRideTwo bikeRide = new BikeRideTwo(bicycle);
+        bikeRide.ride(bicycleSpecification);
+        assertEquals(16, bicycle.currentSpeed());
+
+    }
         @Test
         public void shouldTakeMountainBikeRideThree() {
 
-
             //constructor has 3 parameters
             BicycleSpecification bicycleSpec = new BicycleSpecification(5,3,BicycleType.MountainBike);
-            Bicycle bicycle = new BicycleFromSpec(bicycleSpec);
+            Bicycle bicycleSpecification = new BicycleFromSpec(bicycleSpec);
+            Bicycle bicycle = new MountainBike();
             BikeRideThree bikeRide = new BikeRideThree(bicycle);
-            bikeRide.ride(bicycle);
-            assertEquals(44, bicycle.currentSpeed());
+            bikeRide.ride(bicycleSpecification);
+            assertEquals(49, bicycle.currentSpeed());
         }
         @Test
         public void shouldTakeRoadBikeRideOne() {
 
             //3 parameters
             BicycleSpecification bicycleSpec = new BicycleSpecification(11,4,BicycleType.RoadBike);
-            Bicycle bicycle = new BicycleFromSpec(bicycleSpec);
+            Bicycle bicycleSpecification = new BicycleFromSpec(bicycleSpec);
+            Bicycle bicycle = new RoadBike();
             BikeRide bikeRide = new BikeRide(bicycle);
-            bikeRide.ride(bicycle);
+            bikeRide.ride(bicycleSpecification);
             assertEquals(24, bicycle.currentSpeed());
         }
-
+    @Test
+    public void shouldTakeRoadBikeRideTwo() {
+        //3 parameters
+        BicycleSpecification bicycleSpec = new BicycleSpecification(11,4,BicycleType.RoadBike);
+        Bicycle bicycleSpecification = new BicycleFromSpec(bicycleSpec);
+        Bicycle bicycle = new RoadBike();
+        BikeRideTwo bikeRide = new BikeRideTwo(bicycle);
+        bikeRide.ride(bicycleSpecification);
+        assertEquals(43, bicycle.currentSpeed());
+    }
+    @Test
+    public void shouldTakeRoadBikeRideThree() {
+        //3 parameters
+        BicycleSpecification bicycleSpec = new BicycleSpecification(11,4,BicycleType.RoadBike);
+        Bicycle bicycleSpecification = new BicycleFromSpec(bicycleSpec);
+        Bicycle bicycle = new RoadBike();
+        BikeRideThree bikeRide = new BikeRideThree(bicycle);
+        bikeRide.ride(bicycleSpecification);
+        assertEquals(113, bicycle.currentSpeed());
+    }
+    @Test
+    public void shouldTakeTandemBikeRideOne() {
+        //3 parameters
+        BicycleSpecification bicycleSpec = new BicycleSpecification(12,7,BicycleType.Tandem);
+        Bicycle bicycleSpecification = new BicycleFromSpec(bicycleSpec);
+        Bicycle bicycle = new Tandem();
+        BikeRide bikeRide = new BikeRide(bicycle);
+        bikeRide.ride(bicycleSpecification);
+        assertEquals(13, bicycle.currentSpeed());
+    }
+    @Test
+    public void shouldTakeTandemBikeRideTwo() {
+        //3 parameters
+        BicycleSpecification bicycleSpec = new BicycleSpecification(12,7,BicycleType.Tandem);
+        Bicycle bicycleSpecification = new BicycleFromSpec(bicycleSpec);
+        Bicycle bicycle = new Tandem();
+        BikeRideTwo bikeRide = new BikeRideTwo(bicycle);
+        bikeRide.ride(bicycleSpecification);
+        assertEquals(39, bicycle.currentSpeed());
+    }
+    @Test
+    public void shouldTakeTandemBikeRideThree() {
+        //3 parameters
+        BicycleSpecification bicycleSpec = new BicycleSpecification(12,7,BicycleType.Tandem);
+        Bicycle bicycleSpecification = new BicycleFromSpec(bicycleSpec);
+        Bicycle bicycle = new Tandem();
+        BikeRideThree bikeRide = new BikeRideThree(bicycle);
+        bikeRide.ride(bicycleSpecification);
+        assertEquals(118, bicycle.currentSpeed());
+    }
     }
 
